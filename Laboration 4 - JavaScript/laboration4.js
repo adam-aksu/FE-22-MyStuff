954f72b9ab70fdbf4d7f90144268134faa27bab2
Adm Aksu <adam.extra.it@gmail.com>
2022-02-22 18:44:38 +0100

    Laboration 4 is done.

---
+++ Laboration 4 - JavaScript/laboration4.js
@@ -0,0 +1,30 @@
+
+let num1 = 11, num2 = 22;
+
+
+//	Task 1
+
+console.log(num1 + num2);
+
+console.log(num1 - num2);
+
+console.log(num1 * num2);
+
+// 	Task 2
+//	Solved using template literals
+//	and placeholders.
+ 
+console.log(`${num1} plus ${num2} är: ${num1+num2}`);
+
+//	Task 3
+
+const pers = {
+	fName: "Adam",
+	lName: "Aksu",
+	age: 48
+};
+
+console.log(`Hej, mitt namn är ${pers.fName} ${pers.lName}` +  
+			` och jag är ${pers.age} år gammal.`);
+
+
